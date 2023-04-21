3114e032fce73c2abf2d62cc8f8451657796f512
Dmitry Sky <dmitry.sky@icloud.com>
2023-04-21 14:25:58 -0600

    Stand up basic server

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 import { Configuration, OpenAIApi } from "openai";
+import express from "express";
+import bodyParser from "body-parser"; // middleware
+import cors from "cors"; // middleware
 
 const config = new Configuration({
     // organization: "",
@@ -6,3 +9,21 @@ const config = new Configuration({
 });
 
 const openai = new OpenAIApi(config);
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get("/", async (req, res) => {
+    const completion = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: [{ role: "user", content: "Hello, who are you?" }],
+    });
+    res.json({ completion: completion.data.choices[0].message });
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
